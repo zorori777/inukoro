dc70d17d8abeca9286dda98c7e30ef19f4dd07a0
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-09-11 00:30:57 +0900

    Save breed_list

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,14 @@
 <template>
   <div></div>
 </template>
+
+  <script>
+    import dogApi from '@/api/dog'
+
+    export default {
+        async fetch({store}) {
+          let json = await dogApi.breeds();
+          store.commit('breed_list_update', json)
+        },
+    }
+  </script>
