39be14c2779a2926f7c980473692ed1898ae023a
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-09-10 23:19:14 +0900

    Add AppHeader of component

---
+++ components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nav class="navbar">
+      <div class="container">
+        <div class="navbar-brand">
+          <nuxt-link to="/breeds" class="navbar-item">イヌコロ</nuxt-link>
+          <span class="navbar-burger burger" data-target="navbarMenu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,3 @@
 <template>
-  <div>
-    <nav class="navbar">
-      <div class="container">
-        <div class="navbar-brand">
-          <nuxt-link to="/breeds" class="navbar-item">イヌコロ</nuxt-link>
-          <span class="navbar-burger burger" data-target="navbarMenu">
-            <span></span>
-            <span></span>
-            <span></span>
-          </span>
-        </div>
-      </div>
-    </nav>
-  </div>
+  <div></div>
 </template>
