91693b3b1cba4d92b7ae47e53b9f2469a0ac3095
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-09-13 01:37:23 +0900

    Add index of images

---
+++ pages/dogs/_breed/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="container">
+    <div class="column is-multiline">
+    </div>
+  </section>
+</template>
+
+<script>
+  import dogApi from '@/api/dog';
+  import {mapState} from 'vuex';
+
+  export default {
+    async fetch({store, params}) {
+      const json = await dogApi.dogs(params.breed);
+      store.commit('dog_list_update', json)
+    },
+    computed: mapState(['dog_list']),
+  }
+</script>
+
--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <section class="container">
     <div class="columns is-multiline">
       <div v-for="(item, i) in breed_list" v-bind:key="i" class="column is-2">
-        <a class="button">{{ i }}</a>
+        <nuxt-link :to="{ path: 'dogs/'+ i }" class="button">{{ i }}</nuxt-link>
       </div>
     </div>
   </section>
