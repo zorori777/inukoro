4ab5b264d4211327e0260e8283ea1b9cab894594
zorori777 <e2fde7ded7anxbne05se@yahoo.co.jp>
2018-09-13 01:09:40 +0900

    Add method for getting image

--- api/dog.js
+++ api/dog.js
@@ -12,6 +12,18 @@ class DogApi {
             })
             .catch(e => ({ error: e }));
     }
+    dogs( breed ) {
+        return axios.get(`${this.apiBase}/breed/${breed}/images`)
+        .then(json => {
+            return json.data.message.map((d) => {
+                return {
+                    url: d,
+                    link: 0,
+                };
+            });
+        })
+        .catch(e => ({ error:e }))
+    }
 }
 
 const dogApi = new DogApi();
